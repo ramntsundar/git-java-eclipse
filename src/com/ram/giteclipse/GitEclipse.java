5ede03269ff74d3575e4c5f9cc01a533e8a601f1
Ramsundarbabu <Ramsundarbabu@Ramsundarbabu>
2017-12-21 09:33:54 +0530

    added sysout in GitEclipse files

--- src/com/ram/giteclipse/GitEclipse.java
+++ src/com/ram/giteclipse/GitEclipse.java
@@ -3,8 +3,9 @@ package com.ram.giteclipse;
 public class GitEclipse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			System.out.println("Testing Git from Eclipse");
+			
+			System.out.println("Testing Versioning");
 	}
 
 }
