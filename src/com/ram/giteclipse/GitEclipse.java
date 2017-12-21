e02f156b3ee6783a59f88a6c9cf28ff4601c5138
Ramsundarbabu <Ramsundarbabu@Ramsundarbabu>
2017-12-21 09:34:35 +0530

    added comments section

--- src/com/ram/giteclipse/GitEclipse.java
+++ src/com/ram/giteclipse/GitEclipse.java
@@ -2,6 +2,7 @@ package com.ram.giteclipse;
 
 public class GitEclipse {
 
+	//Added sysout to check multiple commit
 	public static void main(String[] args) {
 			System.out.println("Testing Git from Eclipse");
 			
