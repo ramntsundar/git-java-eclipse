59f6abca0b964fa4c3357529d1cb84f50c2216f4
Ramsundarbabu <ramsb2014@gmail.com>
2017-12-21 13:09:58 +0530

    commiting

--- src/com/ram/giteclipse/GitEclipse.java
+++ src/com/ram/giteclipse/GitEclipse.java
@@ -5,7 +5,7 @@ public class GitEclipse {
 	//Added sysout to check multiple commit
 	public static void main(String[] args) {
 			System.out.println("Testing Git from Eclipse");
-			
+			System.out.println("Delete file from Git and check by commiting code from local");
 			System.out.println("Testing Versioning");
 	}
 
